509a0d75edb2d0b96184c77d0a943a7b7d9b8d4a
Beverly Ann Gupit <Bevzay@users.noreply.github.com>
2020-06-07 13:07:14 +1200

    Sample code exercise of positive, negative and zero

--- src/PositiveNegativeZero.java
+++ src/PositiveNegativeZero.java
@@ -1,4 +1,21 @@
 public class PositiveNegativeZero {
 
-    
+    public static void checkNumber(int number) {
+        if (number > 0) {
+            System.out.println("positive");
+        } else if (number == 0) {
+            System.out.println("equal to 0");
+        } else {
+            System.out.println("negative");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        checkNumber(8);
+        checkNumber(0);
+        checkNumber(-5);
+
+    }
+
 }
